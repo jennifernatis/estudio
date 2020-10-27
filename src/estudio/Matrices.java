a66b715bf752b09b8b70b810a971a540093d1ae7
Jennifer <jennifer.natis@gmail.com>
2020-10-26 19:58:40 -0500

    Puse un mensaje de salida

--- src/estudio/Matrices.java
+++ src/estudio/Matrices.java
@@ -3,6 +3,8 @@ package estudio;
 public class Matrices {
 
 	public static void main(String[] args) {
+		System.out.println("Este es el programa para las matrices");
+		
 		// TODO Auto-generated method stub
 
 	}
